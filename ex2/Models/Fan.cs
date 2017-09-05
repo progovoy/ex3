c31a48cce30fd7b1a01c2ec0bdf055c1ef108849
progovoy <p.rogovoy@gmail.com>
2017-09-05 16:18:22 -0700

    fan works

---
+++ ex2/Models/Fan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ex2.Models
+{
+    public class Fan
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int YearsInClub { get; set; }
+
+        public Fan()
+        {
+            this.FirstName = "XX";
+            this.LastName = "XX";
+            this.YearsInClub = 0;
+            this.Gender = 'F';
+            this.BirthDate = new DateTime();
+        }
+
+        public Fan(string first, string last, char Gender, int years)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.YearsInClub = years;
+            this.Gender = Gender;
+            this.BirthDate = DateTime.Now;
+        }
+    }
+
+    public class FanDBContext : DbContext
+    {
+        public DbSet<Fan> Fans { get; set; }
+    }
+}
